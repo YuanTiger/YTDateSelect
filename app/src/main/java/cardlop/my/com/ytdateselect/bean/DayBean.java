c524584e1353654ecdddb2c0158875e7383fc385
孟远 <mengy@ruwe.cn>
2017-11-22 09:49:29 +0800

    init

---
+++ app/src/main/java/cardlop/my/com/ytdateselect/bean/DayBean.java
@@ -0,0 +1,11 @@
+package cardlop.my.com.ytdateselect.bean;
+
+/**
+ * Author：mengyuan
+ * Date  : 2017/11/22上午9:49
+ * E-Mail:mengyuanzz@126.com
+ * Desc  :一天的Bean对象
+ */
+
+public class DayBean {
+}
---
+++ app/src/main/java/cardlop/my/com/ytdateselect/bean/MonthBean.java
@@ -0,0 +1,15 @@
+package cardlop.my.com.ytdateselect.bean;
+
+import java.io.Serializable;
+
+/**
+ * Author：mengyuan
+ * Date  : 2017/11/22上午9:48
+ * E-Mail:mengyuanzz@126.com
+ * Desc  :一个月的Bean对象
+ */
+
+public class MonthBean implements Serializable {
+    
+
+}
